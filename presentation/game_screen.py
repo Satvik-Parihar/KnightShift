534a8a73ecdbefcae18e00bf12b90a8f56f6a30f
Satvik-Parihar <harishparihar663@gmail.com>
2026-07-21 23:00:38 +0530

    Wire InputHandler into game loop with move highlights

--- presentation/game_screen.py
+++ presentation/game_screen.py
@@ -13,6 +13,7 @@ import pygame
 from config import settings
 from domain.game_state import GameState
 from presentation.board_renderer import BoardRenderer
+from presentation.input_handler import InputHandler
 
 
 class GameScreen:
@@ -33,6 +34,7 @@ class GameScreen:
 
         self._renderer = BoardRenderer()
         self._game_state = GameState()
+        self._input_handler = InputHandler(self._game_state)
 
         self._running = False
 
@@ -50,10 +52,19 @@ class GameScreen:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self._running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                pixel_x, pixel_y = event.pos
+                self._input_handler.handle_click(pixel_x, pixel_y)
 
     def _draw_frame(self):
         self._window.fill(settings.COLOR_PANEL_BACKGROUND)
-        self._renderer.draw_board(self._board_surface, self._game_state)
+        self._renderer.draw_board(
+            self._board_surface,
+            self._game_state,
+            selected_square=self._input_handler.selected_square,
+            legal_move_targets=self._input_handler.legal_move_targets(),
+            last_move=self._input_handler.last_move,
+        )
         self._draw_side_panel_placeholder()
         pygame.display.flip()
 
